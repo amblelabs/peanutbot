9d8deef278c117e4f30725ed03577803ec4e7604
Theo <theo@git.shardmc.su>
2025-07-14 21:18:31 +0700

    Update support.ts

--- src/util/support.ts
+++ src/util/support.ts
@@ -1,4 +1,4 @@
-import config from "../../config.json";
+import config from "config.json";
 
 type SearchResult = [found: boolean, weight: number];
 type SupportResult = [weight: number, message: string];

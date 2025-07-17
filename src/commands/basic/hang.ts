69f5b9ebaed3dc7e1e02724e87e8c7706b57a0ac
Theo <theo@git.shardmc.su>
2025-07-17 15:38:31 +0700

    jelly

---
+++ src/commands/basic/hang.ts
@@ -0,0 +1,19 @@
+import { type Message } from "discord.js";
+import type { CmdData, Ctx } from "~/util/base"
+
+const data: CmdData = {
+    name: 'hang jelly',
+};
+
+function makeReply(): string {
+    return 'https://cdn.discordapp.com/attachments/1213989170964340885/1378558565463101460/Jellys_been_bad.gif';
+}
+
+async function execute(ctx: Ctx, message: Message, args: string[]) {
+    await message.reply(makeReply());
+}
+
+export default {
+    data,
+    execute,
+}

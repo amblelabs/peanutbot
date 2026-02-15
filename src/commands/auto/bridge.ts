5404b4aced9a36477d4e4259e56e07dedb0cea63
Theo <git@theo.is-a.dev>
2026-02-15 03:39:29 +0300

    feat(brdige): replies, fixes

--- src/commands/auto/bridge.ts
+++ src/commands/auto/bridge.ts
@@ -1,5 +1,5 @@
 import config from "config.json";
-import { Events, GuildChannel, WebhookClient } from "discord.js";
+import { Events, GuildChannel, WebhookClient, type APIEmbed } from "discord.js";
 import { Client } from "stoat.js";
 import type { Cmd, Ctx } from "~/util/base";
 import { logger } from "~/util/logger";
@@ -21,7 +21,7 @@ class RevoltWebhookClient {
   private latter: string;
   private baseURL: string;
 
-  constructor(idOrURL: string, instanceURL = "https://app.revolt.chat/api") {
+  constructor(idOrURL: string, instanceURL = "https://api.revolt.chat/") {
     this.latter = idOrURL;
     this.baseURL = instanceURL;
   }
@@ -75,6 +75,8 @@ const stoat2Discord = Object.fromEntries(
   Object.entries(discord2Stoat).map(([key, val]) => [val, key]),
 );
 
+const dismoji2Stoatmoji = Object.entries(config.bridge.emojis);
+const stoatmoji2Dismoji = dismoji2Stoatmoji.map(([key, val]) => [val, key]);
 const client = new Client();
 
 export default {
@@ -96,10 +98,69 @@ export default {
       const webhook = discord2Webhook[discordChannel];
 
       logger.info("(stoat) message: " + message.content);
+
+      let content = message.content;
+
+      for (const [stoatmoji, dismoji] of stoatmoji2Dismoji) {
+        content = content.replaceAll(stoatmoji, dismoji);
+      }
+
+      const embeds: APIEmbed[] = [];
+
+      if (message.replyIds) {
+        const replies = await Promise.all(
+          message.replyIds.map(
+            (val) =>
+              client.messages.get(val) || message.channel?.fetchMessage(val),
+          ),
+        );
+
+        for (const reply of replies) {
+          if (!reply) continue;
+
+          embeds.push({
+            author: {
+              icon_url: message.author.avatarURL || message.avatarURL,
+              name: message.author.displayName || message.username || "unknown",
+            },
+            description: reply.content,
+          });
+        }
+      }
+
+      if (message.mentionIds && message.server) {
+        const members = await Promise.all(
+          message.mentionIds.map(
+            (mention) =>
+              message.server!.getMember(mention) ||
+              message.server!.fetchMember(mention),
+          ),
+        );
+
+        for (const member of members) {
+          content = content.replaceAll(
+            `<@${member.id.user}>`,
+            `\`@${member?.displayName}\``,
+          );
+        }
+      }
+
+      if (embeds.length > 0) {
+        await webhook.send({
+          content: "Replying to:",
+          embeds: embeds,
+          avatarURL: message.author.avatarURL || message.avatarURL,
+          username:
+            (message.author.displayName || message.username || "unknown") +
+            " (stoat)",
+        });
+      }
       await webhook.send({
-        content: message.content,
-        avatarURL: message.author.avatarURL,
-        username: message.author.displayName + " (stoat)",
+        content: content,
+        avatarURL: message.author.avatarURL || message.avatarURL,
+        username:
+          (message.author.displayName || message.username || "unknown") +
+          " (stoat)",
       });
     });
 
@@ -110,13 +171,38 @@ export default {
 
       if (!(event.channelId in discord2Stoat)) return;
 
-      console.log("1");
       const stoatChannel = discord2Stoat[event.channelId];
       const webhook = stoat2Webhook[stoatChannel];
 
+      let content = event.content;
+
+      for (const [dismoji, stoatmoji] of dismoji2Stoatmoji) {
+        content = content.replaceAll(dismoji, stoatmoji);
+      }
+
+      for (const mention of event.mentions.members) {
+        content = content.replaceAll(
+          `<@${mention[0]}>`,
+          `\`@${mention[1].displayName}\``,
+        );
+      }
+
+      if (event.reference) {
+        const reply = await event.fetchReference();
+
+        content =
+          `Reply to \`@${reply.author.displayName}\`\n` +
+          reply.content
+            .split("\n")
+            .map((val) => "> " + val)
+            .join("\n") +
+          "\n" +
+          content;
+      }
+
       logger.info(
         await webhook.send({
-          content: event.content,
+          content: content,
           masquerade: {
             avatar: event.member.displayAvatarURL(),
             name: event.member.displayName + " (discord)",

1f5964678969e247531e6c4180f759fd47681509
Theo <git@theo.is-a.dev>
2026-03-07 16:06:06 +0300

    tweak: more blame testers keywords

--- src/commands/fun/blametesters.ts
+++ src/commands/fun/blametesters.ts
@@ -9,11 +9,15 @@ const data: CmdData = {
 
 async function onMessage(ctx: Ctx, message: Message) {
     if (!message.channel.isSendable()) return;
-    if (!message.mentions.has(ctx.client.user!)) return;
 
     const c = message.content.toLowerCase();
 
-    if (c.includes("who") && (c.includes("responsible") || c.includes("blame")))
+    const responsible = message.mentions.has(ctx.client.user!) && (c.includes("who") && (c.includes("responsible") || c.includes("blame")));
+    const misc = (c.includes("gamebreaking") || c.includes("game-breaking")) && c.includes("bug") 
+        || c.includes("no updates") || c.includes("release was") || c.includes("update was")
+        || (c.includes("release") || c.includes("mod") || c.includes("upate")) && (c.includes("lag") || c.includes("bug"));
+
+    if (responsible || misc)
         message.reply(rnd.pickRandom(config.fun.blame));
 }
 

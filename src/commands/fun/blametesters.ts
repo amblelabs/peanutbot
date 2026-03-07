bb7d2e81c4b7fd5e4bd1522cfe22fe64e7dd6eb3
Theo <git@theo.is-a.dev>
2026-03-07 17:01:20 +0300

    fix: dont wait for fun

--- src/commands/fun/blametesters.ts
+++ src/commands/fun/blametesters.ts
@@ -10,28 +10,32 @@ const data: CmdData = {
 async function onMessage(ctx: Ctx, message: Message) {
     if (!message.channel.isSendable() || message.author.bot) return;
 
-    const c = message.content.toLowerCase();
-
-    const responsible = message.mentions.has(ctx.client.user!) && (c.includes("who") && (c.includes("responsible") || c.includes("blame")));
+    async function process(c: string) {
+      const responsible = message.mentions.has(ctx.client.user!) && (c.includes("who") && (c.includes("responsible") || c.includes("blame")));
     
-    if (responsible) {
-        message.reply(rnd.pickRandom(config.fun.blame.antifun.concat(config.fun.blame.noUpdates)));
-        return;
+      if (responsible) {
+          message.reply(rnd.pickRandom(config.fun.blame.antifun.concat(config.fun.blame.noUpdates)));
+          return;
+      }
+
+      const misc = ((c.includes("gamebreaking") || c.includes("game-breaking") || c.includes("game breaking")) && c.includes("bug"))
+          || (c.includes("release") || c.includes("mod") || c.includes("upate")) && (c.includes("lag") || c.includes("bug"));
+    
+    
+      if (misc) {
+          message.reply(rnd.pickRandom(config.fun.blame.antifun));
+          return;
+      }
+
+      const noUpdates = c.includes("no updates") || c.includes("release was") || c.includes("update was");
+
+      if (noUpdates)
+          message.reply(rnd.pickRandom(config.fun.blame.noUpdates));
+      }
     }
 
-    const misc = ((c.includes("gamebreaking") || c.includes("game-breaking") || c.includes("game breaking")) && c.includes("bug"))
-        || (c.includes("release") || c.includes("mod") || c.includes("upate")) && (c.includes("lag") || c.includes("bug"));
-    
-    
-    if (misc) {
-        message.reply(rnd.pickRandom(config.fun.blame.antifun));
-        return;
-    }
-
-    const noUpdates = c.includes("no updates") || c.includes("release was") || c.includes("update was");
-
-    if (noUpdates)
-        message.reply(rnd.pickRandom(config.fun.blame.noUpdates));
+    const content = message.content.toLowerCase();
+    process(content);
 }
 
 export default {

311ff8994727e7f017b595df5ab05ca8c4d04dbc
Theo <git@theo.is-a.dev>
2026-03-09 22:27:19 +0300

    noask

---
+++ src/commands/support/noask.ts
@@ -0,0 +1,44 @@
+import config from "config.json";
+import type {
+  Interaction,
+  Message,
+  SendableChannels,
+  SharedSlashCommand,
+  SlashCommandBuilder,
+} from "discord.js";
+import type { Cmd, CmdData, Ctx } from "~/util/base";
+
+const data: CmdData = {
+  name: "noask",
+};
+
+function makeReply(): string {
+  return "https://cdn.discordapp.com/attachments/1213989170964340883/1480647617514836238/image.png?ex=69b0700d&is=69af1e8d&hm=3544630a7e734655a9474ef0aadc0c3a0d0b8655a8b5a0faf9e201f4d1182997";
+}
+
+async function execute(
+  ctx: Ctx,
+  message: Message,
+  channel: SendableChannels,
+  args: string[],
+) {
+  await message.reply();
+}
+
+function slash(builder: SlashCommandBuilder): SharedSlashCommand {
+  return builder
+    .setDescription("Don't ask to ask.");
+}
+
+async function onInteraction(ctx: Ctx, interaction: Interaction) {
+  if (!interaction.isChatInputCommand()) return;
+
+  await interaction.reply(makeReply());
+}
+
+export default {
+  data,
+  slash,
+  execute,
+  onInteraction,
+} as Cmd;

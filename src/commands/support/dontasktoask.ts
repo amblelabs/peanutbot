9a8511c07946c53bd7d57fdd3f0a25fe358e8081
Theo <git@theo.is-a.dev>
2026-03-09 22:29:35 +0300

    don't ask to ask, just ask

--- src/commands/support/dontasktoask.ts
+++ src/commands/support/dontasktoask.ts
@@ -9,11 +9,11 @@ import type {
 import type { Cmd, CmdData, Ctx } from "~/util/base";
 
 const data: CmdData = {
-  name: "noask",
+  name: "dontask",
 };
 
 function makeReply(): string {
-  return "https://cdn.discordapp.com/attachments/1213989170964340883/1480647617514836238/image.png?ex=69b0700d&is=69af1e8d&hm=3544630a7e734655a9474ef0aadc0c3a0d0b8655a8b5a0faf9e201f4d1182997";
+  return "[Don't ask to ask](https://dontasktoask.com/)[, just ask](https://cdn.discordapp.com/attachments/1213989170964340883/1480647617514836238/image.png?ex=69b0700d&is=69af1e8d&hm=3544630a7e734655a9474ef0aadc0c3a0d0b8655a8b5a0faf9e201f4d1182997)";
 }
 
 async function execute(

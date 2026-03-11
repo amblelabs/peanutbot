167c391446b1fde3c67e0ad2b1babde674e71309
Theo <git@theo.is-a.dev>
2026-03-12 01:18:20 +0300

    listening

---
+++ src/commands/fun/listening.ts
@@ -0,0 +1,16 @@
+import { type Message, type SendableChannels } from "discord.js";
+import type { Cmd, CmdData, Ctx } from "~/util/base"
+
+const data: CmdData = {
+    name: 'listening',
+};
+
+async function onMessage(ctx: Ctx, message: Message) {
+    if (message.contents.toLowerCase().includes("listening") && message.mentions.has(ctx.client.user!))
+        await message.reply({ stickers: ["1480236131759947868"] });
+}
+
+export default {
+    data,
+    onMessage,
+} as Cmd

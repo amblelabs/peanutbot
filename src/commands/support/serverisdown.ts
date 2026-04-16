7ffeaf7b2d5e474d5871d95313c579eb6f7f752b
Theo <git@theo.is-a.dev>
2026-04-16 23:44:50 +0300

    Update serverisdown.ts

--- src/commands/support/serverisdown.ts
+++ src/commands/support/serverisdown.ts
@@ -6,29 +6,65 @@ import {
   type Message,
   type SendableChannels,
 } from "discord.js";
-import type { Cmd, CmdData, Ctx } from "~/util/base";
+import { format, type Cmd, type CmdData, type Ctx } from "~/util/base";
 
 const data: CmdData = {
   name: "serverisdown",
 };
+
+let cooldown = 0;
 
 function slash(builder: SlashCommandBuilder): SharedSlashCommand {
   return builder.setDescription("Help! The server is down!");
 }
 
 async function onInteraction(ctx: Ctx, interaction: Interaction) {
-  if (!interaction.isChatInputCommand() || !config.support.serverIsDown.enabled)
+  if (
+    !interaction.isChatInputCommand() ||
+    !config.support.serverIsDown.enabled
+  ) {
+    if (interaction.isRepliable())
+      await interaction.reply({
+        content: "Notified MC admins!",
+        flags: ["Ephemeral"],
+      });
     return;
+  }
+
+  if (Date.now() - cooldown < config.support.serverIsDown.cooldown) {
+    const timestamp = `<t:${Math.floor((Date.now() + config.support.serverIsDown.cooldown) / 1000)}:R>`;
+
+    await interaction.reply({
+      content: format(config.support.serverIsDown.format.cooldown, timestamp),
+      flags: ["Ephemeral"],
+    });
+
+    return;
+  }
 
   const channel = await ctx.client.channels.fetch(
     config.support.serverIsDown.channel,
   );
 
-  if (!channel || !channel.isSendable()) return;
+  if (!channel || !channel.isSendable()) {
+    await interaction.reply({
+      content: config.support.serverIsDown.format.error,
+      flags: ["Ephemeral"],
+    });
+
+    return;
+  }
 
   await channel.send(
-    `<@&${config.support.serverIsDown.role}> by <@${interaction.user.id}>`,
+    format(config.support.serverIsDown.format.ping, interaction.user.id),
   );
+
+  await interaction.reply({
+    content: config.support.serverIsDown.format.success,
+    flags: ["Ephemeral"],
+  });
+
+  cooldown = Date.now() + config.support.serverIsDown.cooldown;
 }
 
 export default {

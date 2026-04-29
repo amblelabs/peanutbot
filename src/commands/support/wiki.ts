fb7e7f03e178f5692d834ddaff71987e2b49c208
Theo <git@theo.is-a.dev>
2026-04-29 09:05:24 +0300

    fix: replace old wiki command with a new one

--- src/commands/support/wiki.ts
+++ src/commands/support/wiki.ts
@@ -13,11 +13,7 @@ const data: CmdData = {
 };
 
 function makeReply(type?: string): string {
-  if (type === "stargate" || type === "sg") {
-    return config.support.short.wikiStargate;
-  }
-
-  return config.support.short.wikiAit;
+  return config.support.short.wiki;
 }
 
 async function execute(
@@ -26,29 +22,17 @@ async function execute(
   channel: SendableChannels,
   args: string[],
 ) {
-  await message.reply(makeReply(args[0]));
+  await message.reply(config.support.short.wiki);
 }
 
 function slash(builder: SlashCommandBuilder): SharedSlashCommand {
-  return builder
-    .setDescription("Sends a link to the wiki.")
-    .addStringOption((option) =>
-      option
-        .setName("target")
-        .setDescription("Which wiki?")
-        .setChoices(
-          { name: "Adventures in Time", value: "ait" },
-          { name: "Stargate", value: "stargate" },
-          { name: "Timeless Heroes", value: "th" },
-        ),
-    );
+  return builder.setDescription("Sends a link to the wiki.");
 }
 
 async function onInteraction(ctx: Ctx, interaction: Interaction) {
   if (!interaction.isChatInputCommand()) return;
 
-  const type = interaction.options.getString("target");
-  await interaction.reply(makeReply(type ?? undefined));
+  await interaction.reply(config.support.short.wiki);
 }
 
 export default {

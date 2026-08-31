7e11680383f322586334f5dc81aedac3dbc71acb
vmbbi (Max) <yuhimcheng2@gmail.com>
2026-08-31 13:38:16 +0800

    Update hang.ts

--- src/commands/fun/hang.ts
+++ src/commands/fun/hang.ts
@@ -5,7 +5,10 @@ const data: CmdData = {
     name: 'hang',
 };
 
-function makeReply(): string {
+function makeReplySkye(): string {
+    return 'https://images-ext-1.discordapp.net/external/sqwOuPQPml48TOP_P1sIyee0_fp-UCOJpOi7bdfI728/https/static.klipy.com/ii/4e7bea9f7a3371424e6c16ebc93252fe/6b/f5/XA77J1GDlWiBHmd74.mp4';
+}
+function makeReplyNyx(): string {
     return 'https://cdn.discordapp.com/attachments/1213989170964340885/1378558565463101460/Jellys_been_bad.gif';
 }
 

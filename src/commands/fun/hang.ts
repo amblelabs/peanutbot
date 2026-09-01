d1ffa5e4c435b6389d4088f656d7fc0f2050c5dc
vmbbi (Max) <yuhimcheng2@gmail.com>
2026-09-02 02:06:27 +0800

    Update hang.ts

--- src/commands/fun/hang.ts
+++ src/commands/fun/hang.ts
@@ -15,15 +15,11 @@ function makeReplyNyx(): string {
 async function execute(ctx: Ctx, message: Message, channel: SendableChannels, args: string[]) {
     switch(args[0]) {
         case ('jelly'):
-            await message.reply(makeReplyNyx());
-            break;
         case ('kelly'):
-            await message.reply(makeReplyNyx());
-            break
         case ('nyx'):
             await message.reply(makeReplyNyx());
             break
-case ('skye'):
+        case ('skye'):
             await message.reply(makeReplySkye());
             break
 
